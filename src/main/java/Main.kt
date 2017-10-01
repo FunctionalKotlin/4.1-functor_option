8ee2d0d599b5468684b2c14f51fce48a2b826eeb
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-01 16:37:32 +0200

    (music) Nobody... uh, uh, can break my code... uh, uh, uh, uuuh

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -7,3 +7,12 @@ object None : Option<Nothing>()
 data class Just<out A>(val value: A) : Option<A>()
 
 class TextField(val text: Option<String> = None)
+
+fun send(username: String) {
+    //Logic to send username
+}
+
+fun onTextFieldChange(textField: TextField) = when (textField.text) {
+    is None -> print("TextField is empty")
+    is Just -> send(textField.text.value)
+}
