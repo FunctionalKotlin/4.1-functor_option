20f496cf5f1f8586c454f66c37e2db966b691dd4
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-01 16:39:25 +0200

    Ok... this... can be broken

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,18 +1,37 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+
 sealed class Option<out A>
 
 object None : Option<Nothing>()
 
 data class Just<out A>(val value: A) : Option<A>()
 
-class TextField(val text: Option<String> = None)
+val json = """{
+    "name": "Alex",
+    "accountType": "Premium",
+    "email": "alex@functionalhub.com",
+    "url": "www.functionalhub.com"
+}"""
 
-fun send(username: String) {
-    //Logic to send username
+data class Account(
+    val name: String,
+    val accountType: String,
+    val email: String,
+    val url: String) {
+
+    companion object {
+        fun fromJson(json: String): Account = asJsonObject(json).let {
+            Account(
+                it["name"] as String,
+                it["accountType"] as String,
+                it["email"] as String,
+                it["url"] as String)
+        }
+    }
 }
 
-fun onTextFieldChange(textField: TextField) = when (textField.text) {
-    is None -> print("TextField is empty")
-    is Just -> send(textField.text.value)
-}
+fun asJsonObject(json: String): JsonObject =
+    StringBuilder(json).let { Parser().parse(it) } as JsonObject
